648c0b4a43444c267006b30258160196b0239507
Zakhar Azatian <azatian@mlsdev.com>
2019-10-10 15:35:55 +0300

    remove junk code

--- Player/Sources/MVVM/Player/PlayerViewController.swift
+++ Player/Sources/MVVM/Player/PlayerViewController.swift
@@ -14,44 +14,13 @@ import RxCocoa
 class PlayerViewController: UIViewController {
     
     private let player: PlayerType = Player()
-    var paused = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let tap = UITapGestureRecognizer()
-        view.isUserInteractionEnabled = true
-        view.addGestureRecognizer(tap)
-        
-        tap.rx.event.subscribe(onNext: { [weak self] _ in
-            guard let self = self else { return }
-            self.paused = !self.paused
-            if self.paused {
-                self.player.pause.accept(())
-            } else {
-                self.player.play.accept(())
-            }
-        }) 
-        
-        player.currentTime.emit(onNext: { time in
-            print(time)
-        })
-        
-        player.finishPlaying.emit(onNext: { _ in
-            print("Finished")
-        })
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        if let url = self.getURL() {
-            self.player.startPlayTrack.accept(url)
-        }
-    }
-    
-    func getURL() -> URL? {
-        return Bundle.main.url(forResource: "piano", withExtension: "mp3")
     }
 }
 
