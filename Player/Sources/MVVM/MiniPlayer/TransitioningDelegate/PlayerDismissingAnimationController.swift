fb92786d66d3635a0a7a1035dc7d84db3a7d3b48
Zakhar Azatian <>
2019-11-08 11:30:56 +0200

    refactor of dismiss animation

--- Player/Sources/MVVM/MiniPlayer/TransitioningDelegate/PlayerDismissingAnimationController.swift
+++ Player/Sources/MVVM/MiniPlayer/TransitioningDelegate/PlayerDismissingAnimationController.swift
@@ -11,9 +11,11 @@ import UIKit
 final class PlayerDismissingAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
     
     private weak var miniPlayer: MiniPlayerViewController?
-    private var miniPlayerSnapshotView = UIView()
-    private var playerSnapshotView = UIView()
     
+    private var miniPlayerSnapshotView: UIView?
+    private var playerSnapshotView: UIView?
+    
+    private let coverImageView = UIImageView()
     private let playerBackground = UIView()
     
     init(miniPlayer: MiniPlayerViewController?) {
@@ -30,59 +32,87 @@ final class PlayerDismissingAnimationController: NSObject, UIViewControllerAnima
         }
         
         let containerView = transitionContext.containerView
+        takeSnapshots(playerController: playerController, miniPlayer: miniPlayer)
         
-        containerView.insertSubview(playerBackground, at: 3) // we insert background view below tab bar snapshot
+        guard setupViews(containerView: containerView) else {
+            print("ERROR: Views for animation not found")
+            return
+        }
+        
+        configureViews(playerController: playerController, miniPlayerController: miniPlayer)
+        animate(transitionContext: transitionContext, containerView: containerView, miniPlayer, playerController)
+    }
+    
+    private func takeSnapshots(playerController: PlayerViewController, miniPlayer: MiniPlayerViewController) {
+        playerController.coverImageView.isHidden = true // we hide cover before snapshot
+        let playerSnapshot = playerController.view.snapshotView(afterScreenUpdates: true)
+        self.playerSnapshotView = playerSnapshot
+        
+        miniPlayer.coverImageView.isHidden = true
+        miniPlayerSnapshotView = miniPlayer.view.snapshotView(afterScreenUpdates: true)
+    }
+    
+    private func setupViews(containerView: UIView) -> Bool {
+        guard let miniPlayerSnapShot = miniPlayerSnapshotView, let playerSnapshot = playerSnapshotView else { return false }
+        containerView.insertSubview(playerBackground, at: 3) // TODO: Refactor without magic number 3, beause of tab bar
+        containerView.insertSubview(playerSnapshot, aboveSubview: playerBackground)
+        containerView.insertSubview(miniPlayerSnapShot, aboveSubview: playerSnapshot)
+        containerView.insertSubview(coverImageView, aboveSubview: miniPlayerSnapShot)
+        return true
+    }
+    
+    private func configureViews(playerController: PlayerViewController, miniPlayerController: MiniPlayerViewController) {
+        
+        // Background Configuration
         playerBackground.frame = playerController.view.frame
         playerBackground.backgroundColor = playerController.view.backgroundColor
         
-        playerController.coverImageView.isHidden = true
-        if let playerSnapshot = playerController.view.snapshotView(afterScreenUpdates: true) {
-            containerView.insertSubview(playerSnapshot, aboveSubview: playerBackground)
-            playerSnapshot.frame = playerController.view.frame
-            playerSnapshot.backgroundColor = playerController.view.backgroundColor
-            self.playerSnapshotView = playerSnapshot
-        }
+        // Mini Player Snapshot Configuration
+        miniPlayerSnapshotView?.frame.size = miniPlayerController.view.frame.size
+        miniPlayerSnapshotView?.frame.origin = playerController.view.frame.origin
+        miniPlayerSnapshotView?.alpha = 0.0
         
-        miniPlayer.coverImageView.isHidden = true
-        guard let snapshot = miniPlayer.view.snapshotView(afterScreenUpdates: true) else { return }
-            
-        containerView.insertSubview(snapshot, aboveSubview: playerSnapshotView)
-        snapshot.frame.size = miniPlayer.view.frame.size
-        snapshot.frame.origin = playerController.view.frame.origin
-        snapshot.alpha = 0.0
-        miniPlayerSnapshotView = snapshot
-        
-        let coverImageView = UIImageView()
+        // Cover Image Configuration
         coverImageView.contentMode = .scaleAspectFill
         coverImageView.image = playerController.cover
         coverImageView.frame = playerController.coverImageFrame
         coverImageView.layer.cornerRadius = playerController.coverCornerRadius
         coverImageView.layer.masksToBounds = true
-        containerView.insertSubview(coverImageView, aboveSubview: snapshot)
         
+        // Player Snapshot Configuration
+        playerSnapshotView?.frame = playerController.view.frame
+        playerSnapshotView?.backgroundColor = playerController.view.backgroundColor
+
+    }
+    
+    private func animate(transitionContext: UIViewControllerContextTransitioning, containerView: UIView, _ miniPlayerController: MiniPlayerViewController, _ playerController: PlayerViewController) {
+        
+        let miniPlayerFrame = miniPlayerController.view.frame
         playerController.view.isHidden = true
+        
+        let animation: () -> Void = { [playerBackground, playerSnapshotView, miniPlayerSnapshotView, coverImageView] in
+            playerBackground.frame.origin = miniPlayerFrame.origin
+            playerSnapshotView?.frame.origin.y = playerBackground.frame.origin.y
+            miniPlayerSnapshotView?.frame.origin.y = playerBackground.frame.origin.y
+            coverImageView.frame = miniPlayerController.coverImageView.convert(miniPlayerController.coverImageView.bounds, to: containerView)
+            coverImageView.cornerRadius = miniPlayerController.cornerRadius
+            
+            playerSnapshotView?.alpha = 0.0
+            miniPlayerSnapshotView?.alpha = 1.0
+        }
+        
+        let completion: (Bool) -> Void = {
+            miniPlayerController.coverImageView.isHidden = false
+            transitionContext.completeTransition($0)
+        }
         
         UIView.animate(
             withDuration: transitionDuration(using: transitionContext),
-            delay: 0,
-            usingSpringWithDamping: 1,
-            initialSpringVelocity: 1,
-            options: .curveEaseIn,
-            animations: { [weak playerBackground, weak miniPlayerSnapshotView, weak playerSnapshotView] in
-                playerBackground?.frame.origin = miniPlayer.view.frame.origin
-                playerSnapshotView?.frame.origin.y = playerBackground?.frame.origin.y ?? .zero
-                miniPlayerSnapshotView?.frame.origin.y = playerBackground?.frame.origin.y ?? .zero
-                coverImageView.frame = miniPlayer.coverImageView.convert(miniPlayer.coverImageView.bounds, to: containerView)
-                coverImageView.cornerRadius = miniPlayer.cornerRadius
-                
-                self.playerSnapshotView.alpha = 0.0
-                self.miniPlayerSnapshotView.alpha = 1.0
-                
-                playerController.view.frame.origin = miniPlayer.view.frame.origin
-        }) { finished in
-            miniPlayer.coverImageView.isHidden = false
-            transitionContext.completeTransition(finished)
-        }
+                    delay: 0,
+                    usingSpringWithDamping: 1,
+                    initialSpringVelocity: 1,
+                    options: .curveEaseIn,
+                    animations: animation, completion: completion)
     }
 }
 
